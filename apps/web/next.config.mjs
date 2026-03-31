69d73ec9ddb4d1440784567bee6928f82a852f1d
Nocchia04 <beamon70@outlook.it>
2026-04-01 00:51:37 +0200

    Fix

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@cervello-visivo/shared"],
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
